178f80a84eca5e32ed8e09793fc0a936fe3acaac
divyasirigiri <44464023+divyasirigiri@users.noreply.github.com>
2021-07-12 19:21:40 +0530

    Add files via upload
    modified

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 from logging import debug
 from flask import Flask, render_template, request
-
 from textblob import TextBlob
 from textblob import Word
 
-app1 = Flask(__name__)
+app = Flask(__name__)
 
-
-@app1.route('/')
+@app.route('/')
 def hello():
     return render_template('base.html')
 
-@app1.route('/define',methods = ['POST'])
-def define():
+@app.route('/detect',methods = ['POST'])
+def predict():
     ip_word = request.form.get('ip-word')
     
     res = ' '.join(map(str, Word(ip_word).definitions))
     return render_template('base.html',prediction_text=f'\n the meaning of the word {(ip_word)}  is : {res}')
 
 if __name__=='__main__':
-    app1.run(debug=True)
+    app.run(debug=True)
+
